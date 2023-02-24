9f1bc9d3ccd5f92f4812f9d0b885ac61ba6a2b43
JP-Cardoso <jhonpoal@hotmail.com>
2023-02-24 14:51:06 -0300

    add da função de listar as dividas natabela

--- src/app/components/dashboard/debts/debts.component.ts
+++ src/app/components/dashboard/debts/debts.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ListDebts } from 'src/app/interfaces/listDebts';
+import { ApiService } from 'src/app/services/api.service';
+import { LocalstorageService } from 'src/app/services/localstorage.service';
+import { StoreService } from 'src/app/shared/store.service';
 import { AddDebtsComponent } from '../add-debts/add-debts.component';
 
 @Component({
@@ -7,12 +13,35 @@ import { AddDebtsComponent } from '../add-debts/add-debts.component';
   templateUrl: './debts.component.html',
   styleUrls: ['./debts.component.scss']
 })
-export class DebtsComponent {
-  
-  constructor(
-    private dialog: MatDialog
-  ) {
+export class DebtsComponent implements AfterViewInit{
 
+  user!: string;  
+  loading = false;
+  emptyResult = false;  
+  arrDebts: any[] = [];
+  public dataSource = new MatTableDataSource<any>();
+  @ViewChild('paginator') paginator!: MatPaginator;
+  monthSelected!: string;
+  displayedColums: string[] = [
+    'divida', 'categoria', 'valor', 'dataVencimento', '_id', 'acoes'
+  ]
+
+  constructor(
+    private dialog: MatDialog,
+    private localStorageService: LocalstorageService,
+    private apiService: ApiService,
+    private storeService: StoreService
+
+  ) {
+    this.storeService.getStoreMonth().subscribe(res => {
+      this.monthSelected = res
+    })
+    this.storeService.getStoreDebts().subscribe(res => {
+      if(res) {
+        this.getResgisterDebts(this.monthSelected)
+      }
+    })
+    this.defineInitMonth()
   }
 
   openDialog() {
@@ -20,4 +49,52 @@ export class DebtsComponent {
       width: '600px'
     })
   }
+
+  ngAfterViewInit() {
+    this.getResgisterDebts(this.monthSelected)
+  }
+
+  getResgisterDebts(monthSelected: string) {
+    this.user = this.localStorageService.getLocalStorage('user');
+    this.apiService.getRegisterDebts(monthSelected, this.user)
+      .subscribe((res:ListDebts) => {
+        this.loading = true;
+        let arr: any[] = [];
+
+        if(res.result.length === 0) {
+          this.emptyResult = true;
+          this.arrDebts = []
+        
+        } else {
+          this.emptyResult = false;
+          this.arrDebts = arr;
+        
+          setTimeout(() => {
+            this.dataSource.paginator = this.paginator
+          }, 2001);
+          res.result.forEach((element: any) => {
+            arr.push(element.user.month.listMonth)
+          })
+        }
+
+        setTimeout(() => {
+          this.dataSource.data = arr;
+          this.dataSource.paginator = this.paginator;
+          this.loading = false;
+          console.log('arr -->', arr);
+          
+        }, 2000)
+
+      })
+  }
+
+  defineInitMonth() {
+    let date = new Date();
+    let dateString = date.toLocaleDateString('pt-br', {month: 'long'});
+    let letterDateString = dateString[0].toUpperCase() + dateString.substring(1);
+
+    this.monthSelected == undefined ? (this.monthSelected= letterDateString) : this.monthSelected
+    
+  }
+
 }
